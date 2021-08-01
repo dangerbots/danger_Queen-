a66c186af9f79e0e26c1f00100a258ed1626e67f
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-08-01 12:20:22 +0530

    Add files via upload

---
+++ dreamAngel/modules/about.py
@@ -0,0 +1,16 @@
+__help__ = """
+*dreamAngel - A Powerful Telegram Group Manager 🎶
+
+Powerful Abilities*
+
+• Group Voice Chat Music Play ❤
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developer ꧁𝄇𖤓࿈കൃഷ്ണതുളസ᭄🌙⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ࿈𖤓𝄆꧂
+
+Github [സഖാവ് വാവ](https://github.com/sakhaavvaavaj93)"""
+
+__mod_name__ = "About"
---
+++ dreamAngel/modules/getehi.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import CallbackContext, run_async
+
+from dreamAngel import dispatcher
+from dreamAngel.modules.disable import DisableAbleCommandHandler
+
+EHI_STRINGS = "Here, new ehi files by @ThedreamAngelBot 👸\n\n👉 https://da.gd/yn1NQ"
+
+
+@run_async
+def ehi(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(EHI_STRINGS)
+
+
+EHI_HANDLER = DisableAbleCommandHandler("ehi", ehi)
+dispatcher.add_handler(EHI_HANDLER)
